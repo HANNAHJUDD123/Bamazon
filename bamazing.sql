d03245129547e9ed998cb2db95864fd47e215652
Hannah Judd <Hannah@hannahs-air.uconnect.utah.edu>
2017-12-13 18:26:03 -0700

    fist commit

---
+++ bamazing.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+	product_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(30) NOT NULL,
+	product_price DECIMAL(10,2) NOT NULL,
+	product_department VARCHAR(30) NOT NULL,
+	stock_quantity INTEGER(11) NOT NULL, 
+	PRIMARY KEY (product_id),
+	UNIQUE (product_name)
+);
+
+INSERT INTO products 
+    (product_name, product_department, product_price, stock_quantity) 
+VALUES 
+    ("Zinus Memory Foam", "Home", 300, 40),
+    ("Dove Conditioner", "Cosmetics", 6.25, 627),
+    ('Glad 12 Gal Trash Bags', 'Grocery', 5.99, 300),
+    ('Nike Shorts', 'Clothing', 17.88, 250),
+    ('Fancy Feast Wet Cat Food', 'Pet', 12.50, 163),
+    ('Huggies Diapers', 'Children', 2.75, 476),
+    ('Granny Smith Apples', 'Produce', 2.35, 800),
+    ('5lb Dumb bell', 'Sports', 7.99, 89),
+    ('Charmin Toiler Paper', 'Grocery', 12.99, 575),
+    ('Band Aid', 'Pharmacy', 3.25, 550);
+   
+   
---
+++ costmer.js
@@ -0,0 +1,124 @@
+// Pull in required dependencies
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+
+	user: 'root',
+
+	password: 'root',
+	database: 'bamazon_db',
+	socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+});
+
+function validateInput(value) {
+	let integer = Number.isInteger(parseFloat(value));
+	let sign = Math.sign(value);
+
+	if (integer && (sign === 1)) {
+		return true;
+	} else {
+		return 'Please enter a whole non-zero number.';
+	}
+}
+
+function promptUserPurchase() {
+
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'product_id',
+			message: 'Please enter the Item ID which you would like to purchase.',
+			validate: validateInput,
+			filter: Number
+		},
+		{
+			type: 'input',
+			name: 'quantity',
+			message: 'How many do you need?',
+			validate: validateInput,
+			filter: Number
+		}
+	]).then(function(input) {
+
+		let item = input.product_id;
+		let quantity = input.quantity;
+
+		let queryStr = 'SELECT * FROM products WHERE ?';
+
+		connection.query(queryStr, {product_id: item}, function(err, data) {
+			if (err) throw err;
+
+			if (data.length === 0) {
+				console.log('ERROR: Invalid Item ID. Please select a valid Product ID.');
+				displayInventory();
+
+			} else {
+				let productData = data[0];
+
+				if (quantity <= productData.stock_quantity) {
+					console.log('Congratulations, the product you requested is in stock! Placing order!');
+
+					let updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE product_id = ' + item;
+
+					connection.query(updateQueryStr, function(err, data) {
+						if (err) throw err;
+
+						console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
+						console.log('Thank you for shopping with us!');
+						console.log("\n---------------------------------------------------------------------\n");
+
+		
+						connection.end();
+					})
+				} else {
+					console.log('Sorry, there is not enough product in stock, your order can not be placed as is.');
+					console.log('Please modify your order.');
+					console.log("\n---------------------------------------------------------------------\n");
+
+					displayInventory();
+				}
+			}
+		})
+	})
+}
+
+function displayInventory() {
+	
+	queryStr = 'SELECT * FROM products';
+
+	
+	connection.query(queryStr, function(err, data) {
+		if (err) throw err;
+
+		console.log('Existing Inventory: ');
+		console.log('...................\n');
+
+		let strOut = '';
+		for (let i = 0; i < data.length; i++) {
+			strOut = '';
+			strOut += 'Item ID: ' + data[i].product_id + '  //  ';
+			strOut += 'Product Name: ' + data[i].product_name + '  //  ';
+			strOut += 'Department: ' + data[i].product_department + '  //  ';
+			strOut += 'Price: $' + data[i].product_price + '  //  ';
+			strOut += 'Quantity: ' + data[i].stock_quantity + '\n';
+
+			console.log(strOut);
+		}
+
+	  	console.log("---------------------------------------------------------------------\n");
+
+	
+	  	promptUserPurchase();
+	})
+}
+
+
+function runBamazon() {
+	
+	displayInventory();
+}
+
+runBamazon();
